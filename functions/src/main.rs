ab1dda2420f3843803a6d4a7f7c1f1a0734e74b4
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-11-25 08:19:17 -0800

    Add example of working with functions

---
+++ functions/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello, world!");
+    another_function();
+    another_function2(42);
+
+    let a = sum(10, 5);
+    println!("Sum = {}", a);
+}
+
+fn another_function() {
+    println!("Another function.");
+}
+
+fn another_function2(x: i32) {
+    println!("The value of x = {}", x);
+}
+
+fn sum(x: i32, y: i32) -> i32 {
+    x + y
+}
